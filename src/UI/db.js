bbc0099b8f5cbb0ff9009e38cef460345e8aa9b6
priyankara88 <priyankara.sliit@gmail.com>
2023-06-25 04:46:14 +0530

    add bill calculation

--- src/UI/db.js
+++ src/UI/db.js
@@ -1,25 +1,25 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const mysql = require('mysql');
+window.addEventListener("DOMContentLoaded", () => {
+  const mysql = require("mysql");
 
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: null,
-        database: 'electron_db'
-    });
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: null,
+    database: "electron_db",
+  });
 
-    connection.connect();
+  connection.connect();
 
-    const sql = 'SELECT `name`, `age`, `address`, `other` FROM `test`';
+  const sql = "SELECT `name`, `age`, `address`, `email` FROM `test`";
 
-    connection.query(sql, function(error, results, fields) {
-        if (error) throw error;
+  connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
 
-        const name = results[0].other;
-        console.log(results[0].name);
-        const div = document.getElementById("name");
-        div.innerHTML += name;
-    });
+    const name = results[0].name;
+    console.log(results[0].name);
+    const div = document.getElementById("name");
+    div.innerHTML += name;
+  });
 
-    connection.end();
+  connection.end();
 });
