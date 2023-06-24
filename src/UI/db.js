90be8e7f70ada7a424c5f85c9d5ac6eb3b0385a0
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-06-24 10:54:28 +0530

    db change

--- src/UI/db.js
+++ src/UI/db.js
@@ -1,37 +1,25 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const mysql = require("mysql");
+    const mysql = require("mysql");
 
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: null,
-    database: "electron_db",
-  });
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: null,
+        database: "electron_db",
+    });
 
-  connection.connect();
+    connection.connect();
 
-<<<<<<< HEAD
-  const sql = "SELECT `name`, `age`, `address`, `email` FROM `test`";
-=======
-    const sql = 'SELECT * FROM `user`';
->>>>>>> 495b682ab50b605e5c30c7f6d2aecd40d6a7ac3d
+    const sql = "SELECT `name`, `age`, `address`, `email` FROM `test`";
 
-  connection.query(sql, function (error, results, fields) {
-    if (error) throw error;
+    connection.query(sql, function(error, results, fields) {
+        if (error) throw error;
 
-<<<<<<< HEAD
-    const name = results[0].name;
-    console.log(results[0].name);
-    const div = document.getElementById("name");
-    div.innerHTML += name;
-  });
-=======
         const name = results[0].name;
-        console.log(results[0].id);
+        console.log(results[0].name);
         const div = document.getElementById("name");
         div.innerHTML += name;
     });
->>>>>>> 495b682ab50b605e5c30c7f6d2aecd40d6a7ac3d
 
-  connection.end();
+    connection.end();
 });
